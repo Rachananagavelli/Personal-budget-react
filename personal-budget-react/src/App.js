17278624b904e76c634865980170160c2aafc0f1
Rachananagavelli <rachanareddynagavelli@gmail.com>
2023-10-03 16:05:59 -0400

    initial commit

---
+++ personal-budget-react/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Menu/>
+      <Hero/>
+      <div className="mainContainer">
+        <Routes>
+          <Route path="/about">
+            <AboutPage/>
+          </Route>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+          <Route path="/">
+            <HomePage/>
+          </Route>
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
